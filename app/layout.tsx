dae95ab94e9e7df7bb1934d6efc24a7dc46acab0
Stefano <30480337+snebo@users.noreply.github.com>
2025-03-01 21:41:54 +0100

    add dark mode toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from '@/components/footer';
 import Header from '@/components/header';
+import ThemeSwitch from '@/components/theme-switch';
 import ActiveSessionContextProvider from '@/context/active-session-context';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -20,7 +21,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" className="!scroll-smooth">
-			<body className={` bg-gray-50 text-gray-950 ${inter.className} relative pt-32 sm:pt-44`}>
+			<body
+				className={`${inter.className} bg-gray-50 text-gray-950 relative pt-32 sm:pt-44 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+			>
 				<div className="bg-[#fbcfd1] -z-10 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
 				<div className="bg-[#dbd7fb] -z-10 absolute top-[-6rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 				<ActiveSessionContextProvider>
@@ -29,6 +32,7 @@ export default function RootLayout({
 					<Footer />
 					<Toaster position="top-center" />
 				</ActiveSessionContextProvider>
+				<ThemeSwitch />
 			</body>
 		</html>
 	);
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
 };
 
 export default nextConfig;
+module.exports = {
+	experimental: {
+		nextScriptWorkers: false, // Disable the overlay
+	},
+};
