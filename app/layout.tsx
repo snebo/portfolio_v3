430e5bf8099b6de216b3fb45cfa43d5e54fed8ee
stefano N <30480337+snebo@users.noreply.github.com>
2025-06-08 21:40:14 +0100

    add metadata data and og images

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,63 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: 'Stefano | Portfolio',
-	description: 'Stefano is a fullstack developer with a focus on Backend engineering',
+	description:
+		'Stefano is a fullstack developer with a focus on Backend Development, DevOps, and Cloud Computing. He is passionate about building scalable and efficient systems.',
+	openGraph: {
+		title: 'Stefano | Portfolio',
+		description:
+			'Stefano is a fullstack developer with a focus on Backend Development, DevOps, and Cloud Computing. He is passionate about building scalable and efficient systems.',
+		url: 'https://stefano-nebo.vercel.app/',
+		siteName: 'Stefano | Portfolio',
+		images: [
+			{
+				url: 'https://stefano-nebo.vercel.app/og-image.jpg',
+				width: 1200,
+				height: 630,
+				alt: 'Stefano | Portfolio',
+			},
+		],
+		type: 'website',
+		locale: 'en_US',
+	},
+	twitter: {
+		title: 'Stefano | Portfolio',
+		description: 'Stefano is a fullstack developer with a focus on Backend Development',
+		card: 'summary_large_image',
+		site: '@stefano_dev',
+		creator: '@stefano_dev',
+		images: ['https://stefano-nebo.vercel.app/og-image.jpg'],
+	},
+	keywords: [
+		'Stefano',
+		'Portfolio',
+		'Fullstack Developer',
+		'Backend Development',
+		'DevOps',
+		'Cloud Computing',
+		'Software Engineer',
+		'Web Development',
+		'Programming',
+		'JavaScript',
+		'TypeScript',
+		'Node.js',
+		'React',
+		'Next.js',
+		'Express',
+		'MongoDB',
+		'PostgreSQL',
+		'Docker',
+		'Kubernetes',
+		'AWS',
+		'Azure',
+		'stefano nebo',
+		'snebo',
+		'stefano nebo portfolio',
+		'stefano nebo developer',
+		'stefano nebo fullstack developer',
+		'stefano nebo backend developer',
+		'stefano nebo devops',
+	],
 };
 
 export default function RootLayout({
